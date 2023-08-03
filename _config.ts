d773f75f597a26dfeb04b80fd63fdfcc7f029f79
PickledChair <ubatamamoon@gmail.com>
2023-08-03 13:56:12 +0900

    trivial change

--- _config.ts
+++ _config.ts
@@ -3,8 +3,8 @@ import jsx_preact from "lume/plugins/jsx_preact.ts";
 
 const site = lume();
 
-site.
-  copy("static/img", "img")
+site
+  .copy("static/img", "img")
   .copy("styles/main.css", "main.css")
   .copy("static/manifest.json", "manifest.json")
   .use(jsx_preact());
